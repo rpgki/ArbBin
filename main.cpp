2f40848c683cbbffc84f824a3e99a695a3041b49
a76326 <a76326@ECCI-LBW-105-05.ecci.ucr.ac.cr>
2016-08-11 15:28:12 -0600

    Creación proyecto

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: a76326
+ *
+ * Created on 11 de agosto de 2016, 03:26 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
